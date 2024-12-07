c145abc52d3564814ea07dce314a71a084b4cfb1
ryonakaya <nakayaryo0716@gmail.com>
2024-12-07 15:38:21 +0900

    [add]Add new UUID service

---
+++ app/domain/service/UUIDService.scala
@@ -0,0 +1,11 @@
+package domain.service
+
+trait UUIDService {
+    // UUIDを生成するメソッド
+    // @param: Unit
+
+    // @return
+    // - String
+    //   Stringに型変換されたUUID
+    def generate(): String
+}
