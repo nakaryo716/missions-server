ca3ee0cd0c10e9138de5d37df782cc6064ea25df
ryonakaya <nakayaryo0716@gmail.com>
2024-12-08 22:17:36 +0900

    [fix]Add comment

--- app/infrastructure/service/SaltGenerator.scala
+++ app/infrastructure/service/SaltGenerator.scala
@@ -2,6 +2,7 @@ package infrastructure.service
 
 import java.security.SecureRandom
 
+// SaltStringを生成するオブジェクト
 object SaltGenerator {
   def generateSalt(length: Int): Array[Byte] = {
     val salt = new Array[Byte](length)
